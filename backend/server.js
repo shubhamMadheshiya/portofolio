2764f58c1437df66d8f4b4ef8062f9ce01e98f9f
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-07-01 12:22:53 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -17,29 +17,8 @@ app.use(bodyParser.json());
 
 
 // Route to handle form submission
-app.post("/api/contact", async(req, res) => {
-  const { name, email, message } = req.body;
-  try {
-    const result = await mailgun.sendEmail('contact',name, email, message);
-      return res
-        .status(200)
-        .json({
-          success: true,
-          message: "Your message has been sent successfully!",
-        });
-    
-  } catch (error) {
-     console.log(error);
-     return res
-        .status(500)
-        .json({
-          success: false,
-          message: "Something went wrong. Please try again later.",
-        });
-    
-  }
-
-});
+app.use("/", require("./src/router/root"));
+app.post("/api/contact", require("./src/router/contact"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ backend/src/router/contact.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const keys = require("../config/keys");
+const mailgun = require("../services/mailgun");
+
+router.get("contact", async (req, res) => {
+  const { name, email, message } = req.body;
+  try {
+    const result = await mailgun.sendEmail("contact", name, email, message);
+    return res.status(200).json({
+      success: true,
+      message: "Your message has been sent successfully!",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong. Please try again later.",
+    });
+  }
+});
+
+module.exports = router;
