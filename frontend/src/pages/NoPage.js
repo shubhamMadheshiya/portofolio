ef0eb79b14c72069682c18034f97874688539ad2
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-07-03 11:52:41 +0530

    no page

--- frontend/src/pages/NoPage.js
+++ frontend/src/pages/NoPage.js
@@ -1,11 +1,16 @@
+import { Container, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const NoPage = () => {
   return (
-    <div>
-      404 Not Found
-    </div>
-  )
+    <Container
+      maxWidth="md"
+      
+      sx={{height:'100vh' ,display: "flex", justifyContent: "center", alignItems: "center" }}
+    >
+      <Typography variant='h1'>404 Not Found</Typography>
+    </Container>
+  );
 }
 
 export default NoPage
