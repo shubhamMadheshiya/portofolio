e4560779c746014683b05dd13a7cc9dd7959cc62
Shubham Kumar Maddheshiya <kr.shubhamgupta1999@gmail.com>
2024-09-14 16:17:30 +0530

    add devops certificate

--- frontend/src/data/certificates.js
+++ frontend/src/data/certificates.js
@@ -2,6 +2,18 @@
 import udemy from '../assets/icons/certificate/udemy.svg'
 const certificates = [
   {
+    name: "DevOps Beginners to Advanced with Projects ",
+    id: "UC-a15ce18b-5ef8-4042-85b0-88dec0c112df",
+    logo: udemy,
+    url: "http://ude.my/UC-a15ce18b-5ef8-4042-85b0-88dec0c112df",
+  },
+  {
+    name: "Node.js, Express, MongoDB & More: The Complete Bootcamp",
+    id: "UC-bd986d66-1665-4586-9c54-ad8c8d206801",
+    logo: udemy,
+    url: "http://ude.my/UC-bd986d66-1665-4586-9c54-ad8c8d206801",
+  },
+  {
     name: "Complete Web & Mobile Designer: UI/UX, Figma, +more",
     id: "UC-1ea647fe-6d2b-4905-841b-d771fda78f4d",
     logo: udemy,
@@ -18,12 +30,6 @@ const certificates = [
     id: "UC-f99b6f24-ff0c-46b1-8507-86f913ff8d62",
     logo: udemy,
     url: "http://ude.my/UC-f99b6f24-ff0c-46b1-8507-86f913ff8d62",
-  },
-  {
-    name: "Node.js, Express, MongoDB & More: The Complete Bootcamp",
-    id: "UC-bd986d66-1665-4586-9c54-ad8c8d206801",
-    logo: udemy,
-    url: "http://ude.my/UC-bd986d66-1665-4586-9c54-ad8c8d206801",
   },
 ];
 
