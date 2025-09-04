7b6567c44936ae9ef2f8e0f707f742edef71cb25
Shubham Kumar Maddheshiya <shubham.kumar@alansari.ae>
2025-09-04 16:06:05 +0530

    Handling Mobile UI in react application

---
+++ frontend/src/Hooks/useViewportHeight.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+export const useViewportHeight = () => {
+  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportHeight(window.innerHeight);
+    };
+
+    // Attach the event listener
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return viewportHeight;
+};
--- frontend/src/layout/MainContent .js
+++ frontend/src/layout/MainContent .js
@@ -14,7 +14,7 @@ const MainContent = ({bottomNavHeight,appBarHeight}) => {
         pt: `${appBarHeight + 24}px`,
         overflowY: "auto",
         // maxHeight: `${100-bottomNavHeight-appBarHeight}vh`,
-        height:`calc(100vh - ${bottomNavHeight}px)`,
+        height:`calc(var(--vh, 1vh) * 100 - ${bottomNavHeight}px)`,
         
         "&::-webkit-scrollbar": {
           width: "8px",
