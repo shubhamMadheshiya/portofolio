9aff9d5f60730c23e4bcb984c085b80f569bdf6c
shubhamMadheshiya <kr.shubhamgupta1999@gmail.com>
2025-09-15 02:01:16 +0530

    added download

--- frontend/src/layout/MainContent .js
+++ frontend/src/layout/MainContent .js
@@ -1,42 +1,72 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Download } from "@mui/icons-material";
 import Footer from "./Footer";
+import { Fab, useMediaQuery, useTheme } from "@mui/material";
 import { Stack } from "@mui/material";
 
 const MainContent = ({ bottomNavHeight, appBarHeight }) => {
+  const driveDownloadUrl =
+    "https://drive.google.com/uc?export=download&id=1UZLl8-u5mdVEmdjzaVY34j8it0clqQ0Q";
+
+  const theme = useTheme();
+  const isXs = useMediaQuery(theme.breakpoints.only("xs")); // strictly xs
   return (
-    <Stack
-      //   minHeight="100vh"
-      component="main"
-      sx={{
-        flexGrow: 1,
-        p: 3,
-        pt: `${appBarHeight + 24}px`,
-        overflowY: "auto",
-        // maxHeight: `${100-bottomNavHeight-appBarHeight}vh`,
-        // height: `calc(var(--vh, 1vh) * 100 - ${bottomNavHeight}px)`,
-        height: `calc(100dvh - ${bottomNavHeight}px)`,
+    <>
+      <Stack
+        //   minHeight="100vh"
+        component="main"
+        sx={{
+          display: "relative",
+          flexGrow: 1,
+          p: 3,
+          pt: `${appBarHeight + 24}px`,
+          overflowY: "auto",
+          // maxHeight: `${100-bottomNavHeight-appBarHeight}vh`,
+          // height: `calc(var(--vh, 1vh) * 100 - ${bottomNavHeight}px)`,
+          height: `calc(100dvh - ${bottomNavHeight}px)`,
 
-        "&::-webkit-scrollbar": {
-          width: "8px",
-        },
-        "&::-webkit-scrollbar-track": {
-          background: "#1f2937", // Dark blue background color
-        },
-        "&::-webkit-scrollbar-thumb": {
-          backgroundColor: "#3C454A ", // Dark blue thumb color
-          borderRadius: "4px",
-        },
-        "::-webkit-scrollbar-thumb:hover": {
-          background: "#555",
-        },
-      }}
-    >
-      {/* <DrawerHeader sx={{ display: { xs: "block", sm: "none" } }} /> */}
+          "&::-webkit-scrollbar": {
+            width: "8px",
+          },
+          "&::-webkit-scrollbar-track": {
+            background: "#1f2937", // Dark blue background color
+          },
+          "&::-webkit-scrollbar-thumb": {
+            backgroundColor: "#3C454A ", // Dark blue thumb color
+            borderRadius: "4px",
+          },
+          "::-webkit-scrollbar-thumb:hover": {
+            background: "#555",
+          },
+        }}
+      >
+        {/* <DrawerHeader sx={{ display: { xs: "block", sm: "none" } }} /> */}
 
-      <Outlet />
-      <Footer />
-    </Stack>
+        <Outlet />
+        <Footer />
+        <Fab
+          color="primary"
+          aria-label="download"
+          variant={isXs ? "circular" : "extended"}
+          size={isXs ? "medium" : "large"}
+          component="a"
+          href={driveDownloadUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          sx={{
+            position: "fixed",
+            right: 40,
+            bottom: 80,
+            zIndex: 1000,
+            padding: isXs ? 1 : 2,
+          }}
+        >
+          <Download fontSize={isXs ? "small" : "medium"} />
+          {!isXs && "Download Resume"}
+        </Fab>
+      </Stack>
+    </>
   );
 };
 
